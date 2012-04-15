61f4f11b479658b42d94d76691d8aae844d2d654
Jonathan Moore <jon@jonfm.co.uk>
2012-04-15 20:12:59 +0200

    removed unecessary dependencies and simplified function names

--- server.js
+++ server.js
@@ -14,19 +14,14 @@
  * Module dependencies.
  **/
 
-var http       = require("http"      );
-var formidable = require("formidable");
-var express    = require("express"   );
-var connect    = require("connect"   );
-var fs         = require("fs"        );
-var uuid       = require('node-uuid' );
-var redis      = require("redis"     );
+var express = require("express"   );
+var connect = require("connect"   );
 
 /**
  * This is the module we want to handle our requests.
  **/
 
-var file       = require("./lib/file.js");
+var file    = require("./lib/file.js");
 file.init({
     "uploadDir": process.env.UPLOAD_DIR || __dirname + "/uploads"
 });
@@ -39,8 +34,6 @@ var port          = process.env.PORT            || 3000; //default
 var maxUploadSize = process.env.MAX_UPLOAD_SIZE || 32 * 1024 * 1024; //default
 var app           = express.createServer();
     app.use( connect.logger() );
-var rclient       = redis.createClient();
-    rclient.on("error", function (err) { console.log("Error " + err); });
 
 /**
  * Static path routes
@@ -56,8 +49,8 @@ app.use( "/files",     express.static(file.uploadDir)                         );
  * Dynamic path routes
  **/
 
-app.post( "/upload",      file.upload_file      );
-app.post( "/description", file.save_description );
+app.post( "/upload",      file.upload      );
+app.post( "/description", file.description );
 
 /**
  * Start the app listening on the configured port.
